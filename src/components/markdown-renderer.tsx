1fb9c3be608b1f105d4fcc2ffd29a53c09f44741
enip <enip2473@gmail.com>
2025-07-08 18:18:52 +0800

    feat: integrate react-syntax-highlighter for enhanced code rendering in Markdown

--- src/components/markdown-renderer.tsx
+++ src/components/markdown-renderer.tsx
@@ -2,6 +2,8 @@
 'use client';
 
 import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 type MarkdownRendererProps = {
   content: string;
@@ -9,15 +11,34 @@ type MarkdownRendererProps = {
 
 export function MarkdownRenderer({ content }: MarkdownRendererProps) {
   return (
-    <ReactMarkdown
-      components={{
-        h1: ({node, ...props}) => <h1 className="text-3xl font-bold my-4" {...props} />,
-        h2: ({node, ...props}) => <h2 className="text-2xl font-bold my-3" {...props} />,
-        h3: ({node, ...props}) => <h3 className="text-xl font-bold my-2" {...props} />,
-        h4: ({node, ...props}) => <h4 className="text-lg font-bold my-1" {...props} />,
-        ol: ({node, ...props}) => <ol className="list-decimal pl-5 my-4" {...props} />,
-        ul: ({node, ...props}) => <ul className="list-disc pl-5 my-4" {...props} />,
-        li: ({node, ...props}) => <li className="my-1" {...props} />,
-      }}>{content}</ReactMarkdown>
+    <div className="markdown">
+      <ReactMarkdown
+        components={{
+          code({ node, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || '');
+            return match ? (
+              <SyntaxHighlighter
+                style={atomDark}
+                language={match[1]}
+                PreTag="div"
+              >
+                {String(children).replace(/\n$/, '')}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+          h1: ({node, ...props}) => <h1 className="text-3xl font-bold my-4" {...props} />,
+          h2: ({node, ...props}) => <h2 className="text-2xl font-bold my-3" {...props} />,
+          h3: ({node, ...props}) => <h3 className="text-xl font-bold my-2" {...props} />,
+          h4: ({node, ...props}) => <h4 className="text-lg font-bold my-1" {...props} />,
+          ol: ({node, ...props}) => <ol className="list-decimal pl-5 my-4" {...props} />,
+          ul: ({node, ...props}) => <ul className="list-disc pl-5 my-4" {...props} />,
+          li: ({node, ...props}) => <li className="my-1" {...props} />,
+        }}
+      >{content}</ReactMarkdown>
+    </div>
   );
 }
